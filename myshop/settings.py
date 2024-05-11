5dd26bdb74168ad1f7482f1c62ac60fd5fb168ae
Omar Raafat <omar.raafat@bit68.com>
2024-05-11 03:25:03 +0300

    Creating a coupon app

--- myshop/settings.py
+++ myshop/settings.py
@@ -44,7 +44,8 @@ LOCAL_APPS = [
     "shop.apps.ShopConfig",
     "cart.apps.CartConfig",
     "orders.apps.OrdersConfig",
-    'payment.apps.PaymentConfig',
+    "payment.apps.PaymentConfig",
+    "coupons.apps.CouponsConfig",
 ]
 
 THIRD_PARTY_APPS = [
@@ -131,7 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / "static"
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
